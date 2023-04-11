922bf38e5b6ccd7202a01485c8f7b088eb6409e7
Göröcs Lajos Zsolt <grclajos@gmail.com>
2023-04-11 13:16:59 +0200

    Szövegdobozok igazítása Bootstrap-pel

--- BetterNeptun/resources/views/auth/login.blade.php
+++ BetterNeptun/resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
+                        <div class="row mb-3 align-items-center">
                             <label for="neptunCode" class="col-md-4 col-form-label text-md-end">{{ __('Neptun kód:') }}</label>
 
                             <div class="col-md-6">
@@ -25,7 +25,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="row mb-3 align-items-center">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Jelszó:') }}</label>
 
                             <div class="col-md-6">
