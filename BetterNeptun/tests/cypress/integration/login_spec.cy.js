9c0c184e632d94bcaa6c5bfc3152e495044a1d12
VarroBence <varrobence17@gmail.com>
2023-05-22 01:30:37 +0200

    Cypress - Login empty neptun code

--- BetterNeptun/tests/cypress/integration/login_spec.cy.js
+++ BetterNeptun/tests/cypress/integration/login_spec.cy.js
@@ -1,12 +1,21 @@
 describe('Login', () => {
     it('succesfully loads', () => {
-        cy.visit('/login')
+        cy.visit('/login');
     });
 
-    it.only('succesfull login', () => {
+    it('succesfull login', () => {
         cy.visit('/login');
         cy.get('#neptunCode').type('N4PVV4');
         cy.get('#password').type('123456');
         cy.contains('button', 'Bejelentkezés').click();
+    });
+
+    it('empty neptun code', () => {
+        cy.visit('/login');
+        cy.get('#neptunCode').type('');
+        cy.get('#password').type('123456');
+        cy.contains('button', 'Bejelentkezés').click();
+        cy.on('window:alert',(txt)=>{
+            expect(txt).to.contains('Kérjük, töltse ki ezt a mezőt.')});
     });
 });
