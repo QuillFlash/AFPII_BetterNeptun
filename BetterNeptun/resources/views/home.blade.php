f47e29b83605a201c7ad857d2f4cde34867cefef
Göröcs Lajos Zsolt <grclajos@gmail.com>
2023-05-22 06:21:09 +0200

    Back-enddel való szinkronizálás

--- BetterNeptun/resources/views/home.blade.php
+++ BetterNeptun/resources/views/home.blade.php
@@ -34,7 +34,7 @@
     <div class="container">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-                <h1>Home</h1>
+                <h1>Kezdőoldal</h1>
             </div>
         </div>
     </div>
